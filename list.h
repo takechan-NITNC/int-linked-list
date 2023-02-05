de30453bb672b7965c21af9e9bd5b11957a6c4e9
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-02-05 17:20:17 +0900

    ヘッダファイルの完成

---
+++ list.h
@@ -0,0 +1,20 @@
+#pragma once
+struct node {
+	struct node *previous;
+	struct node *next;
+	int element;
+};
+struct list {
+	struct node *first;
+	struct node *last;
+	int count;
+};
+struct list *list_init();
+struct list *list_init_from_array(int*, int);
+int list_get(struct list*, int);
+void list_set(struct list*, int, int);
+void list_add(struct list*, int);
+void list_insert(struct list*, int, int);
+void list_remove(struct list*, int);
+int list_get_count(struct list*);
+void list_free(struct list*);
